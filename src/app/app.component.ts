c4d92e06d084e19dbd4b57b0bcfbcc40c19cb524
AC-devon <inbox@avaulto.com>
2023-12-08 22:56:01 +0200

    checker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'solblaze-airdrops';
-  airdrop = signal(null) as any
+  airdrop = signal(null) as any;
+  loading = false;
   async calcAirdrop(address:string){
+    this.loading = true
     const validatorScore = (await (await fetch('https://rewards.solblaze.org/api/v1/data')).json()).scores[address];
     const blzeAirdrop = (await (await fetch(`https://rewards.solblaze.org/api/v1/daily_rewards?score=${validatorScore}`)).json()).amount * 7;
-    this.airdrop.set(blzeAirdrop)
+    this.loading = false
+    this.airdrop.set(blzeAirdrop);
   }
 }
