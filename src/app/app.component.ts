ac46738db0e581e397ccc7913628fb040d4706be
AC-devon <inbox@avaulto.com>
2023-12-09 11:47:50 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { inject } from '@vercel/analytics';
 inject({mode: "auto"});
 import va from '@vercel/analytics';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,6 +16,7 @@ export class AppComponent {
   title = 'solblaze-airdrops';
   airdrop = signal(null) as any;
   loading = false;
+  inputVal = ''
   async calcAirdrop(address:string){
     this.loading = true
     const validatorScore = (await (await fetch('https://rewards.solblaze.org/api/v1/data')).json()).scores[address];
